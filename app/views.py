62f36924b839dbf6fd1aface59736c03f4b835b6
Jack Maney <jackmaney@gmail.com>
2014-07-12 21:57:15 -0500

    Flashed messages displaying correctly

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from forms import LoginForm
 
@@ -22,4 +22,8 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login Requested for OpenID="' + form.openid.data + '" remember_me=' + str(form.remember_me.data))
+        return redirect("/index")
+
     return render_template("login.html", title="Sign In", form=form)
