7a9161a9bec1758a1a6e75e01ffd83e501ffbdc9
Jack Maney <jackmaney@gmail.com>
2014-07-13 22:01:41 -0500

    Part VIII Completed

--- app/views.py
+++ app/views.py
@@ -62,6 +62,9 @@ def after_login(resp):
         user = User(nickname=nickname, email=resp.email, role=ROLE_USER)
         db.session.add(user)
         db.session.commit()
+        # Every user follows themselves
+        db.session.add(user.follow(user))
+        db.session.commit()
     remember_me = False
     if 'remember_me' in session:
         remember_me = session["remember_me"]
@@ -73,6 +76,50 @@ def after_login(resp):
 def logout():
     logout_user()
     return redirect(url_for("index"))
+
+@app.route('/follow/<nickname>')
+@login_required
+def follow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+
+    if user is None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+
+    if user == g.user:
+        flash("You can't follow yourself!")
+        return redirect(url_for('user', nickname=nickname))
+
+    u = g.user.follow(user)
+    if u is None:
+        flash('Cannot follow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You are now following ' + nickname + '!')
+    return redirect(url_for('user', nickname=nickname))
+
+@app.route('/unfollow/<nickname>')
+@login_required
+def unfollow(nickname):
+    user = User.query.filter_by(nickname=nickname).first()
+
+    if user is None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+
+    if user == g.user:
+        flash("You can't follow yourself!")
+        return redirect(url_for('user', nickname=nickname))
+
+    u = g.user.unfollow(user)
+    if u is None:
+        flash('Cannot unfollow ' + nickname + '.')
+        return redirect(url_for('user', nickname=nickname))
+    db.session.add(u)
+    db.session.commit()
+    flash('You have stopped following ' + nickname + '.')
+    return redirect(url_for('user', nickname=nickname))
 
 @app.route('/user/<nickname>')
 @login_required
