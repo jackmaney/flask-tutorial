e9b7aab1e70760b24d64605db18e760df6ac2cde
Jack Maney <jackmaney@gmail.com>
2014-07-15 06:00:34 -0500

    Bugfix in timestamp_handler. Got correct imgs in place. I think I'll consider this good enough for Part XII.

--- app/timestamp_handler.py
+++ app/timestamp_handler.py
@@ -4,16 +4,25 @@ import datetime
 def howLongAgo(ts):
     now = datetime.datetime.utcnow()
     seconds = (now - ts).total_seconds()
-    if seconds < 1:
+    if seconds < 3:
         return "just now"
     elif seconds < 60:
         return "%d seconds ago" % int(round(seconds))
     elif seconds < 3600:
         numMinutes = seconds / 60.0
-        return "%d minutes ago" % int(round(numMinutes))
+        if int(round(numMinutes)) > 1:
+            return "%d minutes ago" % int(round(numMinutes))
+        else:
+            return "1 minute ago"
     elif seconds < 3600 * 24:
         numHours = seconds / 3600.0
-        return "%d hours ago" % int(round(numHours))
+        if int(round(numHours)) > 1:
+            return "%d hours ago" % int(round(numHours))
+        else:
+            return "1 hour ago"
     else:
         numDays = seconds / (24 * 3600.0)
-        return "%d days ago" % int(round(numDays))
+        if int(round(numDays)) > 1:
+            return "%d days ago" % int(round(numDays))
+        else:
+            return "1 day ago"
